f6d3d50859d4781c19fff65783d4415c9f886525
Peng Ding <pding.dp@gmail.com>
2019-06-09 00:32:43 -0500

    color class

---
+++ ray/color.go
@@ -0,0 +1,21 @@
+package ray
+
+import "vector"
+
+// Color is a type alias of vector.Vec3
+type Color = vector.Vec3
+
+// R return the 1st channel of c
+func (c *Color) R() float64 {
+	return c.X()
+}
+
+// G returns the 2nd channel of c
+func (c *Color) G() float64 {
+	return c.Y()
+}
+
+// B returns the 3rd channel of c
+func (c *Color) B() float64 {
+	return c.Z()
+}
