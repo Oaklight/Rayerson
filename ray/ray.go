cb31f41e941c8a288fcd8190fd0bcc0a0d67ea9b
Peng Ding <pding.dp@gmail.com>
2019-06-10 22:26:30 -0500

    unexport Ray's internals

--- ray/ray.go
+++ ray/ray.go
@@ -2,30 +2,28 @@ package ray
 
 import "vector"
 
-// Ray comprises of an origin A, and a direction B
+// Ray comprises of an origin, and a direction
 type Ray struct {
-	// A is the origin
-	A *vector.Vec3
-	// B is the direction
-	B *vector.Vec3
+	origin *vector.Vec3
+	direct *vector.Vec3
 }
 
 // NewRay creates and returns a Ray object from given values
 func NewRay(a, b *vector.Vec3) *Ray {
-	return &Ray{A: a, B: b}
+	return &Ray{origin: a, direct: b}
 }
 
 // Origin returns the origin of given Ray object
 func (r *Ray) Origin() *vector.Vec3 {
-	return r.A
+	return r.origin
 }
 
 // Direction returns the direction of given Ray object
 func (r *Ray) Direction() *vector.Vec3 {
-	return r.B
+	return r.direct
 }
 
 // PointAtScale returns a point of t times the given Ray r, along its direction
 func (r *Ray) PointAtScale(t float64) *vector.Vec3 {
-	return vector.Add(r.A, vector.Mut(r.B, t))
+	return vector.Add(r.origin, vector.Mut(r.direct, t))
 }
