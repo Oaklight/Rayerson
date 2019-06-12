4c655a45d7a8cfbeddfb2532603412f387b85d92
Peng Ding <pding.dp@gmail.com>
2019-06-11 22:47:43 -0500

    Export Ray's components, getting rid of tedious functions

--- ray/ray.go
+++ ray/ray.go
@@ -4,26 +4,16 @@ import "vector"
 
 // Ray comprises of an origin, and a direction
 type Ray struct {
-	origin *vector.Vec3
-	direct *vector.Vec3
+	Origin *vector.Vec3
+	Direct *vector.Vec3
 }
 
 // NewRay creates and returns a Ray object from given values
 func NewRay(a, b *vector.Vec3) *Ray {
-	return &Ray{origin: a, direct: b}
-}
-
-// Origin returns the origin of given Ray object
-func (r *Ray) Origin() *vector.Vec3 {
-	return r.origin
-}
-
-// Direction returns the direction of given Ray object
-func (r *Ray) Direction() *vector.Vec3 {
-	return r.direct
+	return &Ray{Origin: a, Direct: b}
 }
 
 // PointAtScale returns a point of t times the given Ray r, along its direction
 func (r *Ray) PointAtScale(t float64) *vector.Vec3 {
-	return vector.Add(r.origin, vector.Mut(r.direct, t))
+	return vector.Add(r.Origin, vector.Mut(r.Direct, t))
 }
