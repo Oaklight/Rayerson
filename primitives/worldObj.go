b18118dc41ab194d0bc7395c6d84e9b3e9d6d270
Peng Ding <pding.dp@gmail.com>
2019-06-11 23:37:45 -0500

    use interface instead of ptr to interface

--- primitives/worldObj.go
+++ primitives/worldObj.go
@@ -3,10 +3,10 @@ package primitives
 import "ray"
 
 // World defines a series of Hitable objects
-type World []*Hitable
+type World []Hitable
 
 // Add appends the given variable(s) to a world list
-func (w *World) Add(hs ...*Hitable) {
+func (w *World) Add(hs ...Hitable) {
 	for _, each := range hs {
 		*w = append(*w, each)
 	}
@@ -27,7 +27,7 @@ func (w *World) Hit(r *ray.Ray, tMin, tMax float64) *Hit {
 			// if some node already intersected with a much nearer object,
 			// closet will be updated and that would block anything farther
 			// to be hit successfully
-			if hit := (*each).Hit(r, tMin, closet); hit != nil {
+			if hit := each.Hit(r, tMin, closet); hit != nil {
 				closet = hit.T
 				record = hit
 			}
