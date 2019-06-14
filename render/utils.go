e029da33c8e7e097216d1dbe2fe61cc781113d2d
Peng Ding <pding.dp@gmail.com>
2019-06-14 15:51:56 -0500

    separate scene related functions into scene.go

--- render/utils.go
+++ render/utils.go
@@ -3,37 +3,16 @@ package render
 import (
 	"bufio"
 	"fmt"
-	"math/rand"
 	"os"
 	pm "primitives"
 	"ray"
 	"runtime"
 	"strconv"
 	"strings"
-	"time"
-	vec3 "vector"
 )
 
-func SceneParser(csvPath string) *pm.World {
-	csvFile, _ := os.Open(csvPath)
-	defer csvFile.Close()
-	scanner := bufio.NewScanner(csvFile)
-	worldObj := pm.World{}
-	for {
-
-		if obj := csvReadline(scanner); obj != nil {
-
-			worldObj.Add(obj)
-
-		} else {
-			break
-		}
-	}
-	return &worldObj
-}
-
-func ArgParse() (nThread int, csvFile string) {
-	helpMsg := `Usage: render [-p=[num of threads]] <csv file>
+func ArgParse() (nThread int, csvFile string, outFile string) {
+	helpMsg := `Usage: render [-p=[num of threads]] <csv file> <output file>
 	<csv file> = The file that contains the batch of images that need to be processed.
 	-p=[num of threads] = An optional flag to run the editor in its parallel version.
 	You also have the option of specifying the number of threads
@@ -44,7 +23,7 @@ func ArgParse() (nThread int, csvFile string) {
 	// widthPtr := flag.Bool("fork", false, "a bool")
 
 	switch len(os.Args) {
-	case 3:
+	case 4:
 		flag := strings.Split(os.Args[1], "=")
 		if len(flag) == 2 {
 			nThread, _ = strconv.Atoi(flag[1])
@@ -54,10 +33,12 @@ func ArgParse() (nThread int, csvFile string) {
 		// in case there is malicious input
 		if nThread > 0 {
 			// TODO: create threads for rendering
+			// initWorkerPool(nThread)
 		}
 		fallthrough
-	case 2:
-		csvFile = os.Args[len(os.Args)-1]
+	case 3:
+		csvFile = os.Args[len(os.Args)-2]
+		outFile = os.Args[len(os.Args)-1]
 	default:
 		fmt.Println(helpMsg)
 		os.Exit(1)
@@ -109,80 +90,6 @@ func csvReadline(csvReader *bufio.Scanner) pm.Hitable {
 // 	jt      jobType
 // }
 
-// RandomScene returns a 'random' scene
-func RandomScene(csvPath string) *pm.World {
-	rand.Seed(time.Now().UnixNano())
+// func initWorkerPool(nThread int) {
 
-	csvFile, err := os.Create(csvPath)
-	if err != nil {
-		panic("File creation failed")
-	}
-	defer csvFile.Close()
-
-	world := pm.World{}
-	floor := pm.NewSphere(0, -1000, -1, 1000, pm.NewDiffuse(ray.NewColor(0.5, 0.5, 0.5)))
-	world.Add(floor)
-	radius := 0.2
-	for a := -11; a < 11; a++ {
-		for b := -11; b < 11; b++ {
-			material := rand.Float64()
-
-			center := vec3.Vec3{
-				X: float64(a) + 0.9*rand.Float64(),
-				Y: radius,
-				Z: float64(b) + 0.9*rand.Float64(),
-			}
-
-			if center.Sub(&vec3.Vec3{4, radius, 0}).Length() > 0.9 {
-				if material < 0.8 {
-					r, g, b := rand.Float64()*rand.Float64(), rand.Float64()*rand.Float64(), rand.Float64()*rand.Float64()
-					diffuse := pm.NewSphere(center.X, center.Y, center.Z, radius,
-						pm.NewDiffuse(&ray.Color{r, g, b}))
-
-					world.Add(diffuse)
-					fmt.Fprintf(csvFile,
-						"%f,%f,%f,%f,%s,%f,%f,%f\n",
-						center.X, center.Y, center.Z, radius, "Diffuse", r, g, b)
-
-				} else if material < 0.95 {
-					r, g, b := 0.5*(1.0+rand.Float64()), 0.5*(1.0+rand.Float64()), 0.5*(1.0+rand.Float64())
-					fuzz := 0.5 + rand.Float64()
-					metal := pm.NewSphere(center.X, center.Y, center.Z, radius,
-						pm.NewMetallic(&ray.Color{r, g, b}, fuzz))
-
-					world.Add(metal)
-					fmt.Fprintf(csvFile,
-						"%f,%f,%f,%f,%s,%f,%f,%f,%f\n",
-						center.X, center.Y, center.Z, radius, "Metallic", r, g, b, fuzz)
-
-				} else {
-					idx := 1.5
-					glass := pm.NewSphere(center.X, center.Y, center.Z, radius,
-						pm.NewDielectric(idx))
-
-					world.Add(glass)
-					fmt.Fprintf(csvFile,
-						"%f,%f,%f,%f,%s,%f\n",
-						center.X, center.Y, center.Z, radius, "Dielectric", idx)
-
-				}
-			}
-		}
-	}
-
-	glass := pm.NewSphere(0, 1, 0, 1.0, pm.NewDielectric(1.5))
-	diffuse := pm.NewSphere(-4, 1, 0, 1.0, pm.NewDiffuse(&ray.Color{0.4, 0, 0.1}))
-	metal := pm.NewSphere(4, 1, 0, 1.0, pm.NewMetallic(&ray.Color{0.7, 0.6, 0.5}, 0))
-
-	world.Add(glass, diffuse, metal)
-	fmt.Fprintf(csvFile,
-		"%f,%f,%f,%f,%s,%f\n",
-		0.0, 1.0, 0.0, 1.0, "Dielectric", 1.5)
-	fmt.Fprintf(csvFile,
-		"%f,%f,%f,%f,%s,%f,%f,%f\n",
-		-4.0, 1.0, 0.0, 1.0, "Diffuse", 0.4, 0.0, 0.1)
-	fmt.Fprintf(csvFile,
-		"%f,%f,%f,%f,%s,%f,%f,%f,%f\n",
-		4.0, 1.0, 0.0, 1.0, "Metallic", 0.7, 0.6, 0.5, 0.0)
-	return &world
-}
+// }
